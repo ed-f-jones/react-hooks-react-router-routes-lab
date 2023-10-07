e9b2b818b1b1f26f63a5c48d9abeb36ee5cb03b1
Edward <ed.f.jones@gmail.com>
2023-10-07 18:42:18 -0400

    almost complete

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,10 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Movies Page</h1>
+    
+  </div>;
 }
 
 export default Movies;
